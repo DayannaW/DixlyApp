47a24f74579022fcea860e48b8104aebb0c8c4c6
Dayanna Vera <dayannavera@MacBook-Air-de-Dayanna.local>
2026-02-04 00:18:55 -0500

    intento 1

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nombre del caché (cambiar la versión cuando actualices la app)
-const CACHE_NAME = 'mi-pwa-cache-v6';
+const CACHE_NAME = 'mi-pwa-cache-v7';
 
 // Archivos que queremos precargar
 const FILES_TO_CACHE = [
