5e4464af315ea02506b91d236c9ddd862b2d52e4
Dayanna Vera <dayannavera@MacBook-Air-de-Dayanna.local>
2026-02-03 23:49:43 -0500

    favicon

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nombre del caché (cambiar la versión cuando actualices la app)
-const CACHE_NAME = 'mi-pwa-cache-v4';
+const CACHE_NAME = 'mi-pwa-cache-v5';
 
 // Archivos que queremos precargar
 const FILES_TO_CACHE = [
@@ -8,6 +8,7 @@ const FILES_TO_CACHE = [
   'inicio.html',
   'historicoInsignias.html',
   'manifest.json',
+  'favicon.ico',
   // Vistas
   'vistas/juego1/index.html',
   'vistas/juego1/juego1nivel1.html',
